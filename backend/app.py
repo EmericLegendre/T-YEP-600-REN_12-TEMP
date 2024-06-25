dfe763865dd778df9938975d36f26ce1031c3232
nathanCahgn <nathan.cahaigne@epitech.eu>
2024-06-25 11:50:31 +0200

    DB connection config

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,14 @@
 from flask import Flask
-from db.dbConfig import dbConfig, db
+from config.dbConfig import dbConfig, db
 from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(dbConfig)
 
-    # init db & migrations
+    # init config & migrations
     db.init_app(app)
     migrate = Migrate(app, db)
-
-    # New test model
-    class Test(db.Model):
-        __tablename__ = 'test'
-        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-        name = db.Column(db.String(50), nullable=False)
-
-        def __repr__(self):
-            return f'<Test {self.id}: {self.name}>'
 
     return app
 
---
+++ backend/config/dbConfig.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+
+class dbConfig:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+db = SQLAlchemy()
