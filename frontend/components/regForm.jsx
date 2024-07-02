b2fd29b37ad39ab29c6915839a4647fcdf332913
camkers <cam.kerserho@gmail.com>
2024-07-02 11:35:34 +0200

    Setup regForm component

---
+++ frontend/components/regForm.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default class regForm extends React component {
+    render() {
+        return (
+            <View style={styles.regFrom}>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    regForm: {
+
+    };
+});
