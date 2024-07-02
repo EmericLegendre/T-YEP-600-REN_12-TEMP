c38b037c7f3ca650f5f8e1df99ac52814c493b03
Mehdi <mehdi3601@hotmail.fr>
2024-07-02 17:07:35 +0200

    Add header on home page and router to profile page

--- frontend/app/(tabs)/_layout.jsx
+++ frontend/app/(tabs)/_layout.jsx
@@ -1,13 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import Colors from "../../constants/Colors"
 
 const Layout = () => {
   return (
-    <Tabs>
-        <Tabs.Screen name='map' />
-        <Tabs.Screen name='home' />
-        <Tabs.Screen name='explore' />
+    <Tabs screenOptions={{
+      tabBarStyle: {
+        backgroundColor: Colors.grey,
+        borderTopWidth: 0,
+        padding: 0,
+        height: 60
+      },
+      tabBarActiveTintColor: Colors.primaryColor,
+      tabBarInactiveTintColor: Colors.white,
+      tabBarLabelStyle: {
+        fontSize: 14,
+        marginBottom: 5
+      }
+    }}>
+        <Tabs.Screen 
+        name='map' 
+        options={{
+          tabBarLabel: "Map",
+          tabBarIcon: ({color}) => (
+          <Ionicons name='map' size={28} color={color} />
+  )}} />
+        <Tabs.Screen 
+        name='home' 
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({color}) => (
+          <Ionicons name='home' size={28} color={color} />
+  )}} />
+        <Tabs.Screen 
+        name='explore' 
+        options={{
+          tabBarLabel: "Explore",
+          tabBarIcon: ({color}) => (
+          <Ionicons name='compass' size={30} color={color} />
+  )}} />
     </Tabs>
   )
 }
