41f52a1f38f1f4aed9a6c4aa37f527a947d0b160
Téophile Chardon <teophile@MacBook-Pro-de-Teophile.local>
2024-07-08 10:54:52 +0200

    FIX : Nomenclature CityInfos.py

--- backend/models/cityInfos.py
+++ backend/models/cityInfos.py
@@ -1,10 +1,22 @@
 from config.dbConfig import db
+from enum import Enum
+
+class CategoryEnum(Enum):
+    CULTURE = 'Culture'
+    TRANSPORT = 'Transport'
+    COOKING = 'Cooking'
+    HEALTH = 'Health'
+
+
 
 class CityInfos(db.Model):
+    """
+    CityInfos model represents the information related to cities, categorized by different aspects.
+    """
     __tablename__ = 'cityInfos'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     cityId = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
-    category = db.Column(db.Enum('Culture', 'Transport', 'Cooking', 'Health'), nullable=False)
+    category = db.Column(db.Enum(CategoryEnum), nullable=False)
     content = db.Column(db.String(255), nullable=False)
 
     def __repr__(self) -> str:
