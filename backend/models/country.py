cad7c8b69fe974e523d16ab687c54da5fdbc0d7d
Téophile Chardon <teophile@MacBook-Pro-de-Teophile.local>
2024-07-08 11:22:27 +0200

    ADD : Flag in country.py

--- backend/models/country.py
+++ backend/models/country.py
@@ -15,9 +15,10 @@ class Country(db.Model):
     country_infos = db.relationship('CountryInfos', backref='country')
     states = db.relationship('State', backref='country')
     cities = db.relationship('City', backref='country')
+    flag = db.Column(db.varchar, nullable=True)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-
+    
 
     def __repr__(self) -> str:
         return (f"Country(id={self.id!r}, name={self.name!r}, continent={self.continent!r}, "
