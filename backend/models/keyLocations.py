73b670d66707581ba5afbc0a1c8102540015994c
Téophile Chardon <teophile@MacBook-Pro-de-Teophile.local>
2024-07-08 10:57:48 +0200

    FIX : Nomenclature keyLocations.py

--- backend/models/keyLocations.py
+++ backend/models/keyLocations.py
@@ -5,9 +5,8 @@ class KeyLocations(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(500), nullable=False)
-    latitude = db.Column(db.String(255), nullable=False)
-    longitude = db.Column(db.String(255), nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
 
     def __repr__(self) -> str:
-
-        return f"User(id={self.id!r}, name={self.name!r}, description={self.description!r}, latitude={self.latitude!r}, longitude={self.longitude!r})"
+        return f"KeyLocations(id={self.id!r}, name={self.name!r}, description={self.description!r}, latitude={self.latitude!r}, longitude={self.longitude!r})"
