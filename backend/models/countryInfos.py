9772d574c1a2ddc46b0a0baa79cd0250ab07563a
Téophile Chardon <teophile@MacBook-Pro-de-Teophile.local>
2024-07-08 10:56:54 +0200

    FIX : Nomenclature countryInfos.py

--- backend/models/countryInfos.py
+++ backend/models/countryInfos.py
@@ -1,11 +1,18 @@
 from config.dbConfig import db
+from enum import Enum
 
+class CategoryEnum(Enum):
+    LANGUAGE = 'Language'
+    CULTURE = 'Culture'
+    LAW = 'Law'
+    COOKING = 'Cooking'
+    HEALTH = 'Health'
 
 class CountryInfos(db.Model):
     __tablename__ = 'countryInfos'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     countryId = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
-    category = db.Column(db.Enum('Language', 'Culture', 'Law', 'Cooking', 'Health'), nullable=False)
+    category = db.Column(db.Enum(CategoryEnum), nullable=False)
     content = db.Column(db.String(255), nullable=False)
 
     def __repr__(self) -> str:
