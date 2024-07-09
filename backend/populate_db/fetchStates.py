c6a081a2517488cb939e3b05aa90e3202501fd05
matteoCogan <matteo.cogan@epitech.eu>
2024-07-09 17:20:55 +0200

    add capital state and get cities when capital is fetch

--- backend/populate_db/fetchStates.py
+++ backend/populate_db/fetchStates.py
@@ -22,58 +22,33 @@ def get_all_country_names():
             
         return country_names
     
-def get_all_capital_state(country_code):
+def get_all_capital_state(country_code,):
     base_url = "http://api.geonames.org/searchJSON"
     params = {
         "country": country_code,
-        "featureCode": "PPLA",
-        "featureCode": "PPLC",
+        "featureCode": ["PPLA", "PPLC"],
         "username": GEONAMES_USERNAME
     }
     query_string = urlencode(params)
-    url = f"{base_url}?{query_string}&featureCode=PPLA&featureCode=PPLC"
+    url = f"{base_url}?{query_string}"
     try:
         response = requests.get(url)
         response.raise_for_status()
-        data = response.json()
+        cities = response.json()
         capitals = {}
-        if 'geonames' not in data:
+        if 'geonames' not in cities:
             return capitals
-        for capital in data['geonames']:
-            adminCode1 = capital.get('adminCode1', None)
+        for city in cities['geonames']:
+            adminCode1 = city.get('adminCode1', None)
             if adminCode1:
-                capitals[adminCode1] = capital['name']
-        return capitals
+                capitals[adminCode1] = city['name']
+        return capitals,cities
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         return None
-    
-
-def get_country_code(encoded_country_name):
-    base_url = "http://api.geonames.org/searchJSON"
-    params = {
-        "q": encoded_country_name,
-        "featureClass": "A", 
-        "featureCode": "PCLI", 
-        "username": GEONAMES_USERNAME
-    }
-
-    try:
-        response = requests.get(base_url, params=params)
-        response.raise_for_status()
-        data = response.json()
-
-        if data['totalResultsCount'] > 0:
-            country_code = data['geonames'][0]['countryCode']
-            return country_code
-        else:
-            return None, None
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None, None
 
 def get_country_states(country_code):
+    all_country_cities = []
     base_url = "http://api.geonames.org/searchJSON"
     params = {
         "country": country_code,
@@ -85,7 +60,7 @@ def get_country_states(country_code):
     response.raise_for_status()
     data = response.json()
     states = []
-    capital_cities = get_all_capital_state(country_code)
+    capital_cities,cities = get_all_capital_state(country_code)
     for state in data['geonames']:
         state_name = state["adminName1"]
         state_population = state.get("population", 0)
@@ -101,24 +76,39 @@ def get_country_states(country_code):
             "adminCode1": adminCode1,
             "countryCode": countryCode
         })
-    return states
+        for cities in cities['geonames']:
+            if cities['adminCode1'] == adminCode1:
+                city_data = {
+                            "name": cities["name"],
+                            "population": cities.get("population", 0),
+                            "state_name": state_name,
+                            "country_name": cities,
+                            "populationName": ""
+                        }
+                all_country_cities.extend(city_data)
+        
+    return states,all_country_cities
 
 def main():
+    all_cities = []
     country_infos = get_all_country_names()
     all_countries_states = []
 
     for country_info in country_infos:
         original_name = country_info['original_name']
         country_code = country_info['countryCode']
-        states = get_country_states(country_code)
+        states,countries_cities = get_country_states(country_code)
         all_countries_states.append({
             "country": original_name,
             "countryCode": country_code,
             "states": states
         })
+        all_cities.extend(countries_cities)
 
-    with open('test.json', 'w', encoding='utf-8') as f:
+    with open('statesData.json', 'w', encoding='utf-8') as f:
         json.dump(all_countries_states, f, indent=2, ensure_ascii=False)
+    
+    
 
 if __name__ == '__main__':
     main()
