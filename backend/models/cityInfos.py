65c91f3e193364738746f2353a5f71482ea98697
nathanCahgn <nathan.cahaigne@epitech.eu>
2024-07-09 11:05:24 +0200

    add country_info route

--- backend/models/cityInfos.py
+++ backend/models/cityInfos.py
@@ -1,12 +1,12 @@
 from config.dbConfig import db
 from enum import Enum
 
+
 class CategoryEnum(Enum):
     CULTURE = 'Culture'
     TRANSPORT = 'Transport'
     COOKING = 'Cooking'
     HEALTH = 'Health'
-
 
 
 class CityInfos(db.Model):
@@ -20,7 +20,6 @@ class CityInfos(db.Model):
     content = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-
 
     def __repr__(self) -> str:
         return f"CityInfos(id={self.id!r}, cityId={self.cityId!r}, category={self.category!r}, content={self.content!r})"
