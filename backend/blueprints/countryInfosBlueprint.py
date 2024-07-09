bb880953f136172431a3df836e3d7af96306f57b
nathanCahgn <nathan.cahaigne@epitech.eu>
2024-07-09 10:23:44 +0200

    add country_info route (working with errors)

--- backend/blueprints/countryInfosBlueprint.py
+++ backend/blueprints/countryInfosBlueprint.py
@@ -1,5 +1,40 @@
-from flask import Blueprint, jsonify
-from models.countryInfos import CountryInfos
+from flask import Blueprint, jsonify, request
+from models.countryInfos import CountryInfos, CategoryEnum
 from config.dbConfig import db
+from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import jwt_required
 
 countryInfosBp = Blueprint('countryInfosBlueprint', __name__)
+
+
+# Country infos routes
+
+@countryInfosBp.route('/add', methods=['POST'])
+@jwt_required()
+def add_country_info():
+    data = request.get_json()
+    required_fields = ['country_id', 'category', 'content']
+
+    # Check for missing fields
+    for field in required_fields:
+        if field not in data:
+            return jsonify({'error': 'Missing required fields'}), 400
+
+    # Check if category is valid
+    try:
+        category = CategoryEnum[data['category']]
+    except KeyError:
+        return jsonify({'error': 'Invalid category'}), 400
+
+    try:
+        new_country_info = CountryInfos(
+            country_id=data['country_id'],
+            category=category,
+            content=data['content']
+        )
+        db.session.add(new_country_info)
+        db.session.commit()
+        return jsonify({'message': 'Country info created successfully', 'country_info': new_country_info.id}), 201
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 400
--- backend/models/countryInfos.py
+++ backend/models/countryInfos.py
@@ -1,12 +1,14 @@
 from config.dbConfig import db
 from enum import Enum
 
+
 class CategoryEnum(Enum):
     LANGUAGE = 'Language'
     CULTURE = 'Culture'
     LAW = 'Law'
     COOKING = 'Cooking'
     HEALTH = 'Health'
+
 
 class CountryInfos(db.Model):
     __tablename__ = 'countryInfos'
@@ -16,7 +18,6 @@ class CountryInfos(db.Model):
     content = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-
 
     def __repr__(self) -> str:
         return (f"CountryInfos(id={self.id!r}, countryId={self.countryId!r}, category={self.category!r}, "
