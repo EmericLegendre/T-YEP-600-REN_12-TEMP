b4ca6d5fbd6e39be4c8b126998cb43d0da1e5c7e
Emeric Legendre <emeric.legendre@epitech.eu>
2024-07-15 10:13:18 +0200

    change variable name

--- backend/models/state.py
+++ backend/models/state.py
@@ -7,17 +7,17 @@ class State(db.Model):
     name = db.Column(db.String(255), nullable=False, unique=True)
     country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=False)
     population = db.Column(db.Integer, nullable=False)  # Changed to Integer
-    populationName = db.Column(db.String(255), nullable=False)
-    regionalCapital = db.Column(db.String(255), nullable=False)
+    population_name = db.Column(db.String(255), nullable=False)
+    regional_capital = db.Column(db.String(255), nullable=False)
 
     cities = db.relationship('City', backref='state', lazy=True)
-    stateInfos = db.relationship('StateInfos', backref='state', lazy=True)
+    state_infos = db.relationship('StateInfos', backref='state', lazy=True)
 
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
 
     def __repr__(self) -> str:
-        return (f"State(id={self.id!r}, name={self.name!r}, countryId={self.countryId!r}, "
-                f"population={self.population!r}, populationName={self.populationName!r}, "
-                f"regionalCapital={self.regionalCapital!r})")
+        return (f"State(id={self.id!r}, name={self.name!r}, countryId={self.country_id!r}, "
+                f"population={self.population!r}, populationName={self.population_name!r}, "
+                f"regionalCapital={self.regional_capital!r})")
