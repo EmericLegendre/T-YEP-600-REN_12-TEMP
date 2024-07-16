cfbc332c5189f22d25a42e3ab3acec9ef665a830
camkers <cam.kerserho@gmail.com>
2024-07-16 11:42:47 +0200

    Fix login page

--- frontend/app/login.jsx
+++ frontend/app/login.jsx
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Pressable } from 'react-native'
 import { Stack, useRouter } from 'expo-router'
 import React from 'react'
 import {useState} from 'react'
 import axios from 'axios'
 import { useNavigation } from '@react-navigation/native'
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
 const login = () => {
   const navigation = useNavigation();
-
   const router = useRouter();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -25,28 +26,32 @@ const login = () => {
           const dataJson = {
               email: email,
               password: password
-              }
+          }
+
+        console.log("je suis dans le try");
         const response = await axios.post('http://localhost:5000/api/users/auth', dataJson );
+        console.log("Response from server:", response.data);
         const { apiToken } = response.data;
+        await localStorage.setItem('token', apiToken);
 
-        localStorage.setItem('token', token);
-
-        navigation.navigate('/tabs/home');
+        console.log("Login successful, navigating to home...");
+        router.push('/home');
       } catch (err) {
         setError('Invalid email or password');
-      }
+    }
   };
 
   return (
       <View style={styles.container}>
         <Text style={styles.header}>Login</Text>
 
-        <TextInput style={styles.textinput} placeholder="Email address" underlineColorAndroid={'transparent'}/>
-        <TextInput style={styles.textinput} placeholder="Password" secureTextEntry={true} underlineColorAndroid={'transparent'}/>
+        <TextInput style={styles.textinput} placeholder="Email address" value={email} onChangeText={setEmail} underlineColorAndroid={'transparent'}/>
+        <TextInput style={styles.textinput} placeholder="Password" svalue={password}  onChangeText={setPassword} secureTextEntry={true} underlineColorAndroid={'transparent'}/>
 
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Pressable style={styles.button} onPress={handleLogin}>
             <Text style={styles.btntext}>Sign in</Text>
-        </TouchableOpacity>
+        </Pressable>
+
       </View>
     );
 }
@@ -58,7 +63,7 @@ const styles = StyleSheet.create({
     flex:1,
     justifyContent: 'center',
     alignItems:'center',
-    backgroundColor:'#F4661B',
+    backgroundColor:'#73FD00',
     paddingLeft: 60,
     paddingRight: 60,
   },
@@ -85,7 +90,7 @@ const styles = StyleSheet.create({
       alignSelf: 'stretch',
       alignItems: 'center',
       padding: 20,
-      backgroundColor: '#ffb74d',
+      backgroundColor: '#FD00CF',
       marginTop: 30,
   },
 });
