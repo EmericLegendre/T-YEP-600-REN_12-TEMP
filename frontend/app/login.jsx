8ebe98f5358959518e77ac80befdf80049b22816
camkers <cam.kerserho@gmail.com>
2024-07-16 15:44:37 +0200

    Fix token storage

--- frontend/app/login.jsx
+++ frontend/app/login.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import {useState} from 'react'
 import axios from 'axios'
 import { useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const login = () => {
   const navigation = useNavigation();
@@ -13,7 +14,6 @@ const login = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-
 
   const handleLogin = async () => {
       if (!email || !password) {
@@ -28,10 +28,10 @@ const login = () => {
           }
 
         console.log("je suis dans le try");
-        const response = await axios.post('http://localhost:5000/api/users/auth', dataJson );
+        const response = await axios.post('http://192.168.56.1:5000/api/users/auth', dataJson );
         console.log("Response from server:", response.data);
         const { apiToken } = response.data;
-        await localStorage.setItem('token', apiToken);
+        await AsyncStorage.setItem('token', apiToken);
 
         console.log("Login successful, navigating to home...");
         router.push('/home');
@@ -45,7 +45,7 @@ const login = () => {
         <Text style={styles.header}>Login</Text>
 
         <TextInput style={styles.textinput} placeholder="Email address" value={email} onChangeText={setEmail} underlineColorAndroid={'transparent'}/>
-        <TextInput style={styles.textinput} placeholder="Password" svalue={password}  onChangeText={setPassword} secureTextEntry={true} underlineColorAndroid={'transparent'}/>
+        <TextInput style={styles.textinput} placeholder="Password" value={password}  onChangeText={setPassword} secureTextEntry={true} underlineColorAndroid={'transparent'}/>
 
         <Pressable style={styles.button} onPress={handleLogin}>
             <Text style={styles.btntext}>Sign in</Text>
