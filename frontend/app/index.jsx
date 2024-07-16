86f7a4402e5c55ddb31d515ff80f02f55d35b538
camkers <cam.kerserho@gmail.com>
2024-07-16 14:34:11 +0200

    Add checkLoggedIn on connexion

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,7 +1,37 @@
-import { Redirect } from "expo-router";
+import React, { useEffect } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import { Stack, useRouter } from 'expo-router'
 
 const StartPage = () => {
-  return <Redirect href="/register" />
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  useEffect(() => {
+    checkLoggedIn();
+  }, []);
+
+  const checkLoggedIn = async () => {
+      try {
+        const apiToken = await AsyncStorage.getItem('token');
+
+        if (apiToken) {
+          router.push('/home');
+        } else {
+          router.push('/register');
+        }
+      } catch (error) {
+        console.error('Error checking login status:', error);
+      }
+    };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size="large" color="#0000ff" />
+      <Text>Checking login status...</Text>
+    </View>
+  );
 };
 
 export default StartPage;
