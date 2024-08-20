1a1979a6b89dc13f199490172efdb3289821a848
nathanCahgn <nathan.cahaigne@epitech.eu>
2024-08-20 14:32:17 +0200

    keyLocations model update

--- backend/models/keyLocations.py
+++ backend/models/keyLocations.py
@@ -5,9 +5,7 @@ class KeyLocations(db.Model):
     __tablename__ = 'keyLocations'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    latitude = db.Column(db.Float, nullable=False)
-    longitude = db.Column(db.Float, nullable=False)
+    place_id = db.Column(db.String, nullable=False)
 
     tripKeyLocations = db.relationship('TripKeyLocations', backref='keyLocations', lazy=True)
 
@@ -15,4 +13,4 @@ class KeyLocations(db.Model):
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     def __repr__(self) -> str:
-        return f"KeyLocations(id={self.id!r}, name={self.name!r}, description={self.description!r}, latitude={self.latitude!r}, longitude={self.longitude!r})"
+        return f"KeyLocations(id={self.id!r}, name={self.name!r}, place_id={self.place_id!r})"
