196b1680b49ccb8d67c7757f1119dd525bd9f418
camkers <cam.kerserho@gmail.com>
2024-09-01 14:09:07 +0200

    Fix header shown

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
-      <Stack.Screen name="profile" options={{ headerShown: false }}/>
+      <Stack.Screen name="profile" options={{ headerShown: true }}/>
     </Stack>
   );
 }
